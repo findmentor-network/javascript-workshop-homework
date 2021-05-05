2a491eefac194ec2e9c940e0001e5af356446b86
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-05 23:24:52 +0300

    Update User Model

--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,3 +1,24 @@
-export class User {
+import { Room } from '../room/room'
 
+export class User {
+  socketId: string;
+  name: string;
+  room: Room;
+  constructor (socketId: string, name: string) {
+    this.socketId = socketId
+    this.name = name
+    this.room = {}
+  }
+
+  static all () {
+
+  }
+
+  static get (id: string) {
+
+  }
+
+  getRoom () {
+    return this.room
+  }
 }
