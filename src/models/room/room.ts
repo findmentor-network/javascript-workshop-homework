6cac61127d34cf1624fec8b4c936ead0b47f84ef
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-06 17:49:20 +0300

    Add amIHost function for room class

--- src/models/room/room.ts
+++ src/models/room/room.ts
@@ -2,22 +2,34 @@ import { User } from '../user/user'
 export class Room {
   name: string;
   members: User[]
-
+  round: number
   constructor (name:string) {
     this.name = name
     this.members = []
+    this.round = 1
   }
 
   static allRooms () {
 
   }
 
-  join (user:User) {
+  join (user: User) {
+    this.members.push(user)
     // If someone in the room
-    if (this.members.length === 0) return this
+    if (this.members[0] === user) {
+      return this
+    }
 
     // If room empty
     const host = this.members[0]
     return `${host.name} is in the room`
+  }
+
+  amIHost (user: User) {
+    // If User is the host
+    if (this.members[0] === user) return true
+
+    // If user not the host
+    return false
   }
 }
