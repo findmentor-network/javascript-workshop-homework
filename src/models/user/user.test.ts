2480af6e2544fc77710cb0f9c356028ff2970c39
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-06 17:28:47 +0300

    Add test for User.all() function

--- src/models/user/user.test.ts
+++ src/models/user/user.test.ts
@@ -3,7 +3,9 @@ import { User } from './user'
 
 describe('[user.ts]', () => {
   it('creates instance of user', () => {
-    const user = new User()
+    const user = new User('sadi', '1')
+    const user2 = new User('kaan', '1')
     expect(user instanceof User).toBe(true)
+    expect(User.all()).toStrictEqual([user, user2])
   })
 })
