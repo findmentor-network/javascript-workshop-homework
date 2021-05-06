060492e9084ea4e60e4b7fb8ae2e49596aff5012
Kaan Ersoy <kaanersoy12@gmail.com>
2021-05-06 11:02:14 +0300

    Update room class and methods

--- src/models/room/room.ts
+++ src/models/room/room.ts
@@ -1,3 +1,23 @@
+import { User } from '../user/user'
 export class Room {
+  name: string;
+  members: User[]
 
+  constructor (name:string) {
+    this.name = name
+    this.members = []
+  }
+
+  static allRooms () {
+
+  }
+
+  join (user:User) {
+    // If someone in the room
+    if (this.members.length === 0) return this
+
+    // If room empty
+    const host = this.members[0]
+    return `${host.name} is in the room`
+  }
 }
